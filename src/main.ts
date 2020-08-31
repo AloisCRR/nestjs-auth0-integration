6eb17e90723d2a8ab278f3a875383cc80f39ebbd
Alois Carrera <aloiscarrera@gmail.com>
2020-08-31 17:20:44 -0500

    Config for input validation
    Input validation is made with class-validator

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+import { ValidationPipe } from '@nestjs/common';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.enableCors();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  await app.listen(process.env.PORT);
 }
 bootstrap();
--- src/note.ts
+++ src/note.ts
@@ -1,7 +1,7 @@
-
+import { IsString, IsOptional } from 'class-validator';
 export class Note {
-    readonly id: string;
-    readonly title: string;
-    readonly note: string;
-    readonly user_id: string;
+  @IsString() @IsOptional() readonly id: string;
+  @IsString() readonly title: string;
+  @IsString() readonly note: string;
+  @IsString() readonly user_id: string;
 }
